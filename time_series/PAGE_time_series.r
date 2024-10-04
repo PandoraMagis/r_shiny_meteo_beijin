ecf703444278e4aa051c930397fd5bdf7e6d7bc7
Magister-Magnus <papail.my@gmail.com>
2024-10-05 01:28:56 +0200

    time series page entry points

--- time_series/PAGE_time_series.r
+++
@@ -1,8 +0,0 @@
-
-
-# Need the map
-
-# need two time selector - maybe choising an other desing so it's easilly
-
-# one with dr who box in original and one bigger on the insdide (i'm freestyling rn let's be honest)
-
--- time_series/SERVER_time_series.r
+++ time_series/SERVER_time_series.r
@@ -1,65 +1,33 @@
-time_stamp = function (year_start, month_start, day_start, hour_start, year_stop, month_stop, day_stop, hour_stop) {
-  if (
-    ( year_start < year_stop ) |
-    ( year_start == year_stop &&  month_start < month_stop) | 
-    ( year_start == year_stop &&  month_start == month_stop && day_start < day_stop) | # nolint
-    ( year_start == year_stop &&  month_start == month_stop && day_start == day_stop && hour_start < hour_stop ) # nolint
-  ){
-    # space is neggatif
-    # TODO - print the error to user
-  } else {
-  
-#   mutate(datetime = ISOdatetime(year, month, day, hour, 0, 0)) %>%  
-    
-  #airbnb_listings %>%
-  #  filter(year %in% year_start : yeat_stop) %>%
-  #  filter(month %in% month_start : month_stop) %>%
-  #  filter(day)
-  #}
-  
-  df <- data.frame(
-    year = c(2023, 2023, 2023, 2024, 2024, 2024),
-    month = c(8, 9, 10, 1, 2, 3),
-    day = c(15, 25, 5, 10, 20, 30),
-    hour = c(12, 14, 16, 18, 20, 22)
-  )
-  # Filter by year range first
-  year_start = 2023
-  year_stop = 2024
-  month_start = 8
-  month_stop = 2
-  df %>% 
-    filter( year >= year_start & year <= year_stop ) %>%
-    filter(
-        (year_start == year_stop & month_start <= month & month <= month_stop) |
-        # If it's the start year, filter from m_start
-        (year == year_start & year != year_stop & month >= month_start) |
-        # If it's the start year, filter from m_start
-        (year != year_start & year == year_stop & month <= month_stop) |
-        # For years in between, include all months
-        (year > year_start & year < year_stop)
-    ) %>%
-    filter(
-        (month_start == month_stop & day_start <= day & day <= day_stop) |
-        # If it's the start month, filter from m_start
-        (month == month_start & month != month_stop & day >= day_start) |
-        # If it's the start month, filter from m_start
-        (month != month_start & month == month_stop & day <= day_stop) |
-        # For years in between, include all months
-        (month > month_start & month < month_stop)
-    ) %>%
-    filter(
-        (day_start == day_stop & hours_start <= hours & hours <= hours_stop) |
-        # If it's the start day, filter from m_start
-        (day == day_start & day != day_stop & hours >= hours_start) |
-        # If it's the start day, filter from m_start
-        (day != day_start & day == day_stop & hours <= hours_stop) |
-        # For years in between, include all hourss
-        (day > day_start & day < day_stop)
-    )
-  }
-    
-    
-   
-  
-}
+# relativ services and fonctions logics
+source("./time_series/functions/date_hell_format.r", local = TRUE)
+
+# Server routes
+
+# develop output
+output$develop_output <- renderText({
+    test_value <- FALSE    
+    if ( test_value ) { 
+        paste("omg it's broken", test_value) 
+    }else{
+        "everything's fine :)"
+    }
+})
+
+
+
+# date selection
+
+# copy paste of how it's manage in 1st part - great to have vars name right at proximity
+    # req(
+    #     input$map_marker_click,
+    #     input$district_radio, 
+    #     input$month_radio, 
+    #     input$custom_slider, 
+    #     input$custom_slider_hour
+    # )
+    # # Include hour filter as well
+    # row <- df[df$station == input$map_marker_click$id &
+    #             df$year == as.integer(input$district_radio) &
+    #             df$month == match(input$month_radio,mois) &
+    #             df$day == input$custom_slider &
+    #             df$hour == input$custom_slider_hour, ]
